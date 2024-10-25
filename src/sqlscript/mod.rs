4a972d96630507e08a1cdb6e42241e11e2b99e61
chenota <achenot@mines.edu>
2024-10-25 00:21:26 -0600

    feat: Added token enum, lexer struct and some base code

--- src/sqlscript/mod.rs
+++ src/sqlscript/mod.rs
@@ -1,5 +1,20 @@
 mod lexer {
-
+    pub enum Token {
+        PlusKw,
+        Integer(i64)
+    }
+    pub struct Lexer {
+        stream: String,
+        pos: u64
+    }
+    impl Lexer {
+        fn produce(&mut self) -> (Token, u64) {
+            (Token::PlusKw, 0)
+        }
+        fn reset(&mut self) -> () {
+            self.pos = 0;
+        }
+    }
 }
 
 pub mod parser {
