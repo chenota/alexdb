914ae60d532af90bfe113943ea0f35c3b2ad3cd5
Alex Chenot <alexchenot5@gmail.com>
2024-11-07 13:12:51 -0700

    test: Added more basic column tests, all pass

--- src/storage/table.rs
+++ src/storage/table.rs
@@ -77,9 +77,8 @@ pub mod table {
 #[cfg(test)]
 mod table_tests {
     use super::*;
-    use super::super::column::generic::DataContainer;
     use crate::sqlscript::lexer::lexer;
-    use crate::storage::column::generic::ColumnContainer;
+    use super::super::column::generic::{DataContainer, ColumnContainer};
     #[test]
     fn test_bool_column() -> Result<(), String> {
         // Setup
@@ -110,6 +109,197 @@ mod table_tests {
             ColumnContainer::BooleanColumn(x) => {
                 match x.as_ref().extract()[0] {
                     None => assert!(true),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        Ok(())
+    }
+    #[test]
+    fn test_int_column() -> Result<(), String> {
+        // Setup
+        let mut test_table = table::Table::new();
+        let col_name1 = "Test1".to_string();
+        let col_name2 = "Test2".to_string();
+        // Create new columns
+        test_table.add_column(&col_name1, lexer::ColType::Integer);
+        test_table.add_column(&col_name2, lexer::ColType::Integer);
+        // Create row
+        let row1 = vec![
+            DataContainer::Int(Some(100)),
+            DataContainer::Int(None)
+        ];
+        // Add row to table
+        test_table.add_row(row1);
+        // Assert correct values
+        match test_table.get_column(&col_name1) {
+            ColumnContainer::IntColumn(x) => {
+                match x.as_ref().extract()[0] {
+                    Some(y) => assert_eq!(y, 100),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        match test_table.get_column(&col_name2) {
+            ColumnContainer::IntColumn(x) => {
+                match x.as_ref().extract()[0] {
+                    None => assert!(true),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        Ok(())
+    }
+    #[test]
+    fn test_float_column() -> Result<(), String> {
+        // Setup
+        let mut test_table = table::Table::new();
+        let col_name1 = "Test1".to_string();
+        let col_name2 = "Test2".to_string();
+        // Create new columns
+        test_table.add_column(&col_name1, lexer::ColType::Float);
+        test_table.add_column(&col_name2, lexer::ColType::Float);
+        // Create row
+        let row1 = vec![
+            DataContainer::Float(Some(12.5)),
+            DataContainer::Float(None)
+        ];
+        // Add row to table
+        test_table.add_row(row1);
+        // Assert correct values
+        match test_table.get_column(&col_name1) {
+            ColumnContainer::FloatColumn(x) => {
+                match x.as_ref().extract()[0] {
+                    Some(y) => assert_eq!(y, 12.5),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        match test_table.get_column(&col_name2) {
+            ColumnContainer::FloatColumn(x) => {
+                match x.as_ref().extract()[0] {
+                    None => assert!(true),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        Ok(())
+    }
+    #[test]
+    fn test_str_column() -> Result<(), String> {
+        // Setup
+        let mut test_table = table::Table::new();
+        let col_name1 = "Test1".to_string();
+        let col_name2 = "Test2".to_string();
+        // Create new columns
+        test_table.add_column(&col_name1, lexer::ColType::String);
+        test_table.add_column(&col_name2, lexer::ColType::String);
+        // Create row
+        let row1 = vec![
+            DataContainer::String(Some("Hello".to_string())),
+            DataContainer::String(None)
+        ];
+        // Add row to table
+        test_table.add_row(row1);
+        // Assert correct values
+        match test_table.get_column(&col_name1) {
+            ColumnContainer::StringColumn(x) => {
+                match &x.as_ref().extract()[0] {
+                    Some(y) => assert_eq!(y, "Hello"),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        match test_table.get_column(&col_name2) {
+            ColumnContainer::StringColumn(x) => {
+                match x.as_ref().extract()[0] {
+                    None => assert!(true),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        Ok(())
+    }
+    #[test]
+    fn test_mixed_column() -> Result<(), String> {
+        // Setup
+        let mut test_table = table::Table::new();
+        let col_name1 = "Test1".to_string();
+        let col_name2 = "Test2".to_string();
+        // Create new columns
+        test_table.add_column(&col_name1, lexer::ColType::Integer);
+        test_table.add_column(&col_name2, lexer::ColType::String);
+        // Create row
+        let row1 = vec![
+            DataContainer::Int(Some(112)),
+            DataContainer::String(Some("Hello".to_string()))
+        ];
+        // Add row to table
+        test_table.add_row(row1);
+        // Assert correct values
+        match test_table.get_column(&col_name1) {
+            ColumnContainer::IntColumn(x) => {
+                match &x.as_ref().extract()[0] {
+                    Some(y) => assert_eq!(*y, 112),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        match test_table.get_column(&col_name2) {
+            ColumnContainer::StringColumn(x) => {
+                match &x.as_ref().extract()[0] {
+                    Some(y) => assert_eq!(y, "Hello"),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        Ok(())
+    }
+    #[test]
+    fn test_multi_row() -> Result<(), String> {
+        // Setup
+        let mut test_table = table::Table::new();
+        let col_name1 = "Test1".to_string();
+        let col_name2 = "Test2".to_string();
+        // Create new columns
+        test_table.add_column(&col_name1, lexer::ColType::Integer);
+        test_table.add_column(&col_name2, lexer::ColType::String);
+        // Create row
+        let row1 = vec![
+            DataContainer::Int(Some(112)),
+            DataContainer::String(Some("Hello".to_string()))
+        ];
+        // Create row
+        let row2 = vec![
+            DataContainer::Int(Some(119)),
+            DataContainer::String(Some("Goodbye".to_string()))
+        ];
+        // Add rows to table
+        test_table.add_row(row1);
+        test_table.add_row(row2);
+        // Assert correct values
+        match test_table.get_column(&col_name1) {
+            ColumnContainer::IntColumn(x) => {
+                match &x.as_ref().extract()[0] {
+                    Some(y) => assert_eq!(*y, 112),
+                    _ => assert!(false)
+                }
+            },
+            _ => assert!(false)
+        }
+        match test_table.get_column(&col_name2) {
+            ColumnContainer::StringColumn(x) => {
+                match &x.as_ref().extract()[1] {
+                    Some(y) => assert_eq!(y, "Goodbye"),
                     _ => assert!(false)
                 }
             },
